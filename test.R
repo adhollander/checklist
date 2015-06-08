f736db672e70f9a1cadd355d616281bc2a74bd39
Allan Hollander <adhollander@ucdavis.edu>
2015-06-07 17:17:59 -0700

    Added GPL license; worked on "excludeds" bug.

---
+++ test.R
@@ -0,0 +1,2 @@
+createchecklist(indvect2, makeissuevect(levels(integrateds), integratedonly=T), issueindmat2, "14", "1845, 2042, 89, 448")
+createchecklist(indvect2, makeissuevect(levels(integrateds), integratedonly=T), issueindmat2, "14", "89, 1153")
